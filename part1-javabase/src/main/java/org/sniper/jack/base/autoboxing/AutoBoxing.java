efcbd7655d2b937abda69803b14abce25a7b10a4
SniperJack <yufanwind@gmail.com>
2018-04-04 17:02:33 +0800

    java base of the autoBoxing

---
+++ part1-javabase/src/main/java/org/sniper/jack/base/autoboxing/AutoBoxing.java
@@ -0,0 +1,18 @@
+package org.sniper.jack.base.autoboxing;
+
+public class AutoBoxing {
+
+
+    public static void main(String[] args) {
+        Integer a = 1000;
+        Integer b = 1000;
+
+
+        System.out.println(a == b);
+
+        Double c = 10.1;
+        Double d = 10.1;
+
+        System.out.println(c == d);
+    }
+}
