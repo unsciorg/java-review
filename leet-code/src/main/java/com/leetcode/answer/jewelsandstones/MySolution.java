c62d8e16f9e6a372790200bf08605ca3725c0feb
wind.yu <fengfeng.yu@kutesmart.com>
2018-04-09 23:31:39 +0800

    add a question of jewels and stones

---
+++ leet-code/src/main/java/com/leetcode/answer/jewelsandstones/MySolution.java
@@ -0,0 +1,22 @@
+package com.leetcode.answer.jewelsandstones;
+
+public class MySolution {
+
+    public int numJewelsInStones(String J, String S) {
+        char[] myStones = S.toCharArray();
+        int count = 0;
+        for (int i = 0; i < myStones.length; i++) {
+            if (J.contains(Character.toString(myStones[i]))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new MySolution().numJewelsInStones("aA","aAAbbbb"));
+    }
+
+
+}
